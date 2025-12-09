3fa300119ecef229918aab7f47c2ca8d4ad039df
bsenkel <93093303+bsenkel@users.noreply.github.com>
2025-12-09 21:52:25 +0100

    refactor part one (day05)

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,25 +1,73 @@
 advent_of_code::solution!(5);
 
+#[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Debug)]
+struct Range {
+    start: u64,
+    end: u64,
+}
+
+impl Range {
+    fn from_str(s: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let parts: Vec<&str> = s.split('-').collect();
+        if parts.len() != 2 {
+            return Err("Invalid format".into());
+        }
+
+        Ok(Range {
+            start: parts[0].parse()?,
+            end: parts[1].parse()?,
+        })
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    let mut fresh_ingredient_ranges = Vec::<&str>::new();
+    let mut fresh_ingredient_ranges = vec![];
     let mut ingredient_ids = Vec::<u64>::new();
     let mut separator = false;
+    let mut fresh_count = 0;
 
     for line in input.lines() {
-        if !separator {
-            fresh_ingredient_ranges.push(line);
-        }
         if line.is_empty() {
             separator = true;
             continue;
         }
+        if !separator {
+            if let Ok(range) = Range::from_str(line) {
+                fresh_ingredient_ranges.push(range);
+            }
+        }
 
-        // start here to process available ingredient IDs
-        ingredient_ids.push(line.parse::<u64>().unwrap());
-        // ...
+        if let Ok(num) = line.parse::<u64>() {
+            ingredient_ids.push(num);
+        }
     }
 
-    Some(0)
+    // TODO: sort ranges and merge
+    fresh_ingredient_ranges.sort();
+
+    println!("{:?}", fresh_ingredient_ranges);
+    println!("Lines: {}", fresh_ingredient_ranges.len());
+
+    println!("{:?}", ingredient_ids);
+    println!("Lines: {}", ingredient_ids.len());
+
+    for id in ingredient_ids {
+        let mut found = false;
+        for range in fresh_ingredient_ranges.iter() {
+            for i in range.start..=range.end {
+                if i == id {
+                    fresh_count += 1;
+                    found = true;
+                    break;
+                }
+            }
+            if found {
+                break;
+            }
+        }
+    }
+
+    Some(fresh_count)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
